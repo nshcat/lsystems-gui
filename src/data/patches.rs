7e3a6fede37f68ee5d9fb726d1eaefaf20dbe6e4
nshcat <nshcat@users.noreply.github.com>
2019-12-13 19:21:16 +0100

    Implemented evalutation function for bezier patches

--- src/data/patches.rs
+++ src/data/patches.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 use serde_derive::*;
-use nalgebra_glm::Vec3;
+use nalgebra_glm::{Mat4, Vec3};
+extern crate nalgebra;
 
 /// Data of a single bezier curve in a bezier patch
 #[derive(Serialize, Deserialize, Clone)]
@@ -35,6 +36,58 @@ impl BezierPatchParameters {
                 BezierCurveParameters::empty()
             ]
         }
+    }
+
+    /// Evalute the patch at given UV position in [0,1]x[0,1].
+    pub fn evaluate(&self, u: f32, v: f32) -> Vec3 {
+        let curve0 = &self.curves[0];
+        let curve1 = &self.curves[1];
+        let curve2 = &self.curves[2];
+        let curve3 = &self.curves[3];
+
+        type MatType = nalgebra::Matrix<f32, nalgebra::Dynamic, nalgebra::Dynamic, nalgebra::VecStorage<f32, nalgebra::Dynamic, nalgebra::Dynamic>>;
+
+        let GBx = MatType::from_row_slice(4, 4,
+            &[curve0.control_points[0].z, curve0.control_points[1].z, curve0.control_points[2].z, curve0.control_points[3].z, 
+            curve1.control_points[0].z, curve1.control_points[1].z, curve1.control_points[2].z, curve1.control_points[3].z, 
+            curve2.control_points[0].z, curve2.control_points[1].z, curve2.control_points[2].z, curve2.control_points[3].z, 
+            curve3.control_points[0].z, curve3.control_points[1].z, curve3.control_points[2].z, curve3.control_points[3].z]
+        );
+
+        let GBy = MatType::from_row_slice(4, 4,
+            &[curve0.control_points[0].y, curve0.control_points[1].y, curve0.control_points[2].y, curve0.control_points[3].y, 
+            curve1.control_points[0].y, curve1.control_points[1].y, curve1.control_points[2].y, curve1.control_points[3].y, 
+            curve2.control_points[0].y, curve2.control_points[1].y, curve2.control_points[2].y, curve2.control_points[3].y, 
+            curve3.control_points[0].y, curve3.control_points[1].y, curve3.control_points[2].y, curve3.control_points[3].y] 
+        );
+
+        let GBz = MatType::from_row_slice(4, 4,
+            &[curve0.control_points[0].z, curve0.control_points[1].z, curve0.control_points[2].z, curve0.control_points[3].z, 
+            curve1.control_points[0].z, curve1.control_points[1].z, curve1.control_points[2].z, curve1.control_points[3].z, 
+            curve2.control_points[0].z, curve2.control_points[1].z, curve2.control_points[2].z, curve2.control_points[3].z, 
+            curve3.control_points[0].z, curve3.control_points[1].z, curve3.control_points[2].z, curve3.control_points[3].z], 
+        );
+
+        let U = MatType::from_row_slice(1, 4,
+            &[u.powf(3.0), u.powf(2.0), u, 1.0]
+        );
+
+        let Vt = MatType::from_row_slice(4, 1,
+            &[v.powf(3.0), v.powf(2.0), v, 1.0]
+        );
+
+        let Mb = MatType::from_row_slice(4, 4,
+            &[  -1.0, 3.0, -3.0, 1.0,
+                3.0, -6.0, 3.0, 0.0,
+                -3.0, 3.0, 0.0, 0.0,
+                1.0, 0.0, 0.0, 0.0]
+        );
+
+        let Mbt = Mb.transpose();
+
+        let resultX = &U * &Mb * &GBx * &Mbt * &Vt;
+        let resultY = &U * &Mb * &GBy * &Mbt * &Vt;
+        let resultZ = &U * &Mb * &GBz * &Mbt * &Vt;
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,17 @@ mod data;
 mod scene;
 
 use crate::data::*;
+use crate::data::patches::*;
 use crate::scene::*;
 use crate::scene::lsystem::*;
 
 fn main() {
+    let test = BezierPatchParameters::empty();
+    println!("{}", test.evaluate(0.0, 0.0));
+
+
+
+
 	let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3)); 
 
