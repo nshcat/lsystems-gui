8160f42faf7fd98e82e8231af0e7c5295b930369
d_boen02 <d_boen02@wwu.de>
2019-12-18 19:32:15 +0100

    Some changes to model

--- src/rendering/model.rs
+++ src/rendering/model.rs
@@ -4,12 +4,41 @@ use crate::rendering::meshes::*;
 use crate::rendering::traits::*;
 use std::rc::*;
 
-/// A model is a mesh combined with a model space transformation.
-#[derive(Clone)]
+/// Mesh manager that can either take full ownership of a meshes or just store a reference
+/// counted pointer to one
+enum MeshStorage {
+	/// Storage manages ref counted pointers to meshes.
+	/// This allows meshes to be shared between multiple models.
+	RefCounted{ meshes: Vec<Rc<Box<Mesh>>> },
+	/// The storage is the sole owner of the meshes.
+	Owned{ meshes: Vec<Mesh> }
+}
+
+impl Render for MeshStorage {
+	fn render(&self, rp: &mut RenderParameters) {
+		match self {
+			MeshStorage::RefCounted{ meshes } => {
+				for mesh in meshes {
+					mesh.render(rp);
+				}
+			},
+			MeshStorage::Owned{ meshes } => {
+				for mesh in meshes {
+					mesh.render(rp);
+				}
+			}
+		}
+	}
+}
+
+
+/// A model is a set of meshes combined with a model transformation matrix.
+/// The meshes can either be owned or referenced via Rc pointers, which allows sharing
+/// of meshes between multiple models.
 pub struct Model {
-    /// The mesh associated with this model
-    pub mesh: Rc<Box<Mesh>>,
-    /// The model transformation
+    /// Storage instance managing the meshes associated with this model
+    pub storage: MeshStorage,
+    /// The model transformation matrix. Converts local model space into world space.
     pub transform: Mat4
 }
 
@@ -27,21 +56,37 @@ impl Render for Model {
         params.push_matrix();
         params.add_matrix(&self.transform);
 
-        self.mesh.render(params);
+        self.storage.render(params);
 
         params.pop_matrix();
     }
 }
 
-
+/// A multi model is a collection of multiple models that form one logical unit. By using
+/// the transformation matrix of the multi model, hierachical rendering may be implemented.
+/// The difference to a Model containing multiple meshes is that the various models
+/// can, in this case, have different model transformation matrices.
 pub struct MultiModel {
-    pub models: Vec<Model>
+	/// Collection of sub models.
+    pub models: Vec<Model>,
+    /// The model matrix that will be applied to all sub models, in addition to their
+    /// specific model matrices.
+    pub transforma: Mat4
+}
+
+impl MultiModel {
+	pub fn new( models.. mutable slice
 }
 
 impl Render for MultiModel {
     fn render(&self, params: &mut RenderParameters) {
+    	params.push_matrix();
+    	params.add_matrix(&self.transform);
+    	
         for model in &self.models {
             model.render(params);
         }
+        
+        params.pop_matrix();
     }
 }
